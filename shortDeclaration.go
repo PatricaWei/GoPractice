2db51a4a1ad07a521607ab69e8bb971da0ddd192
Patricia Wei <patricia.wei.2201@gmail.com>
2017-06-15 16:22:13 -0700

    Create shortDeclaration.go

---
+++ shortDeclaration.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+  var aNum1, aNum2 int = 1, 2
+  aNum3 := 3
+  aBool1, aBool2, aString1 := true, false, "no!"
+
+  fmt.Println(aNum1, aNum2, aNum3, aBool1, aBool2, aString1)
+}
